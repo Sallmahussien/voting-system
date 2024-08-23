dfe639b0c9f58e3ec157faee098b69f7e2cde1fd
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-08-23 11:42:00 +0300

    feat[model]: Add method to get vote by `userId` and `topicId`

--- backend/src/models/vote.dao.js
+++ backend/src/models/vote.dao.js
@@ -1,11 +1,20 @@
 import prisma from "./client.db.js";
 
 class VoteDao {
-  static async craeteVote(voteData) {
+  static async createVote(voteData) {
     return prisma.vote.create({
       data: voteData,
     });
   }
-}
+
+  static async getVoteByUserAndTopic(userId, topicId) {
+    return prisma.vote.findFirst({
+      where: {
+        userId,
+        topicId
+      }
+    });
+  }
+ }
 
 export default VoteDao;
