5fc5b886b52fd994cd27a587dc9ee4598f649470
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-08-24 23:05:33 +0300

    shared[feat]: Implement shared login component

---
+++ frontend/src/app/shared/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  @Output() submit = new EventEmitter<FormGroup>();
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.submit.emit(this.loginForm);
+  }
+}
