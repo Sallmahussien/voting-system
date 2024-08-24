f49438dc964c6cf50212d92e06ecceedb51a26a9
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-08-24 23:06:05 +0300

    feat[interceptor]: Implement authorization interceptor

---
+++ frontend/src/app/auth.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CookieService } from './services/cookie.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private cookieService: CookieService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.cookieService.getCookie('auth_token');
+
+    let authReq = req;
+    if (token) {
+      authReq = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
+  
+    return next.handle(authReq);
+  }
+}
